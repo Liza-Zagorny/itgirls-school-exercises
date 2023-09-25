056d50fd1abc726418b3092a2f1279421bd1f343
Liza Neiman <5gb.place.for.love@gmail.com>
2023-09-25 18:56:43 +0300

    merge sort of words length

--- src/itgirlsschool/lizaneiman/week8/_main/Main8.java
+++ src/itgirlsschool/lizaneiman/week8/_main/Main8.java
@@ -1,16 +1,15 @@
 package _main;
 
 import service.SortingService;
-
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
+
 
 public class Main8 {
     public static void main(String[] args) {
         SortingService solution = new SortingService();
         // Exercise 1 - Bobble Sort
-        ArrayList<Map.Entry<String,Double>> a = new ArrayList<>();
+        System.out.println("Exercise 1 - Bobble Sort");
         HashMap<String, Double> scores = new HashMap<>();
         scores.put("Иванов", 4.5);
         scores.put("Петров", 3.7);
@@ -18,11 +17,18 @@ public class Main8 {
         scores.put("Козлов", 5.0);
         scores.put("Смирнов", 3.9);
         solution.sortByScore(scores);
-
         // Exercise 2 - Quick Sort
+        System.out.println("Exercise 2 - Quick Sort");
         String[] fruits = {"apple", "orange", "grape", "banana"};
+        System.out.println("Original array: " + Arrays.toString(fruits));
+        solution.sortByAbc(fruits, 0, fruits.length - 1);
+        System.out.println("Sorted array: " + Arrays.toString(fruits));
         // Exercise 3 - Merge Sort
+        System.out.println("Exercise 3 - Merge Sort");
         String[] objects = {"cat", "orange", "elephant", "bird"};
+        System.out.println("Original array: " + Arrays.toString(objects));
+        String[] result = solution.mergeSort(objects);
+        System.out.println("Sorted array: " + Arrays.toString(result));
         // Exercise 4 - Insertion sort
         // Exercise 5 - Selection Sort
         String[] formulas = {"5+6+32=43", "1+1=2", "(3+5)*4*21=483", "((21-20)*(32-30))/2=1"};
--- src/itgirlsschool/lizaneiman/week8/service/SortingService.java
+++ src/itgirlsschool/lizaneiman/week8/service/SortingService.java
@@ -1,15 +1,11 @@
 package service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class SortingService {
     public void sortByScore(HashMap<String, Double> scores) {
-        System.out.println(scores);
         List<Map.Entry<String, Double>> list = new ArrayList<Map.Entry<String, Double>>(scores.entrySet());
-        System.out.println(list);
+        System.out.println("Original list: " + list);
         for (int i = 0; i < list.size() - 1; i++) {
             for (int j = 0; j < list.size() - i - 1; j++) {
                 Map.Entry<String, Double> current = list.get(j);
@@ -22,20 +18,20 @@ public class SortingService {
                 }
             }
         }
-        System.out.println(list);
+        System.out.println("Sorted list: " + list);
     }
 
     public void sortByAbc(String[] wordsArr, int lowIndex, int highIndex) {
         if (wordsArr.length == 0 || lowIndex >= highIndex) return;
+
         int middleIndex = lowIndex + (highIndex - lowIndex) / 2;
         String borderWord = wordsArr[middleIndex];
-        // 0 + (3-0)/2 ~2
         int i = lowIndex, j = highIndex;
         while (i <= j) {
-            while (is1SmallerThan2(wordsArr[i],borderWord)) {
+            while (is1SmallerThan2(wordsArr[i], borderWord)) {
                 i++;
             }
-            while (!is1SmallerThan2(wordsArr[j],borderWord)) {
+            while (!is1SmallerThan2(wordsArr[j], borderWord)) {
                 j--;
             }
             if (i <= j) {
@@ -47,12 +43,58 @@ public class SortingService {
             }
         }
         if (lowIndex < j) sortByAbc(wordsArr, lowIndex, j);
-
     }
 
     private boolean is1SmallerThan2(String word1, String word2) {
+        if (Objects.equals(word1, word2)) return false;
         int minLength = Math.min(word1.length(), word2.length());
+        boolean result = false;
+        for (int i = 0; i < minLength; i++) {
+            if (word1.charAt(i) < word2.charAt(i)) {
+                result = true;
+                break;
+            } else if (word1.charAt(i) > word2.charAt(i)) {
+                // result is still false
+                break;
+            }
+        }
+        return result;
+    }
 
-        return true;
+    public String[] mergeSort (String[] arr) {
+        String[] buffer1 = Arrays.copyOf(arr, arr.length);
+        String[] buffer2 = new String[arr.length];
+        return sortByWordLength(buffer1, buffer2, 0, arr.length);
+    }
+    public String[] sortByWordLength(String[] buffer1, String[] buffer2, int startIndex, int endIndex) {
+        if (startIndex >= endIndex - 1) return buffer1;
+
+        int middleIndex = startIndex + (endIndex - startIndex) / 2;
+        String[] subArray1 = sortByWordLength(buffer1, buffer2, startIndex, middleIndex);
+        String[] subArray2 = sortByWordLength(buffer1, buffer2, middleIndex, endIndex);
+
+        int i1 = startIndex, i2 = middleIndex, destIndex = startIndex;
+        String[] result = ((subArray1 == buffer1) ? buffer2 : buffer1);
+        while (i1 < middleIndex && i2 < endIndex) {
+            if (subArray1[i1].length() < subArray2[i2].length()) {
+                result[destIndex] = subArray1[i1];
+                i1++;
+            } else {
+                result[destIndex] = subArray1[i2];
+                i2++;
+            }
+            destIndex++;
+        }
+        while (i1 < middleIndex) {
+            result[destIndex] = subArray1[i1];
+            i1++;
+            destIndex++;
+        }
+        while (i2 < endIndex) {
+            result[destIndex] = subArray1[i2];
+            i2++;
+            destIndex++;
+        }
+        return result;
     }
 }
