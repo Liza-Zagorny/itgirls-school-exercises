189703ecc94b6ad681e0ccb133821bf88b08b373
Liza Neiman <5gb.place.for.love@gmail.com>
2023-10-01 01:28:36 +0300

    week9 create animals and service

---
+++ src/itgirlsschool/lizaneiman/week9/_main/Main9.java
@@ -0,0 +1,23 @@
+package _main;
+
+import entity.Animal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main9 {
+    public static void main(String[] args) {
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Animal("cow", 1));
+        animals.add(new Animal("pig", 2));
+        animals.add(new Animal("cat", 3));
+        animals.add(new Animal("dog", 4));
+        animals.add(new Animal("duck", 5));
+        animals.add(new Animal("tiger", 6));
+        animals.add(new Animal("zebra", 7));
+        animals.add(new Animal("elephant", 8));
+        animals.add(new Animal("hippo", 9));
+        animals.add(new Animal("horse", 10));
+
+    }
+}
